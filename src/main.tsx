bc6a5fc2fa389def63cef0af9a6f259bbaf9cec0
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-03-05 23:23:41 +0900

    feat: Topページを作成

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
+import { TopPage } from './pages/TopPage';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold text-lime-500 underline">Hello World</h1>
-    </div>
+    <TopPage/>
   </React.StrictMode>,
 )
