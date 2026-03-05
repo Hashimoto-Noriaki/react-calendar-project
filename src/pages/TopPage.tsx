13e94405fc977e7d3b0b856f183ecce11d812432
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-03-05 23:40:26 +0900

    refactor: ヘッダー部分のレイアウトとボタンをコンポーネントに切り出した

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,28 +1,16 @@
+import { PrimaryBtn } from '../shared/components/organisms/PrimaryBtn'
+import { NotLoginLayout } from '../shared/components/organisms/NotLoginLayout'
+
 export const TopPage = () => {
     return (
-        <div className="relative">
-            <header className="bg-white leading-12.5 fixed top-0 left-0 right-0">
-                <div className="container mx-auto flex justify-between">
-                    <p className="logo">スケジュール管理APP</p>
-                    <nav>
-                        <ul className="flex gap-5 text-lime-800">
-                            <li>利用説明</li>
-                            <li>ログイン</li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
-            <main className="pt-12.5 bg-linear-to-r from-lime-100 to-lime-200 h-screen flex flex-col justify-center items-center">
-                <div className="text-center">
-                    <h1 className="text-7xl logo">スケジュール管理APP</h1>
-                    <p className="pt-[10vh] text-5xl">お互いのスケジュールを管理するアプリです</p>
-                    <div className="pt-[20vh]">
-                        <button className="bg-lime-800 text-white p-4 text-lg rounded-lg">
-                            ログイン
-                        </button>
-                    </div>
-                </div>
-            </main>
-        </div>
+        <NotLoginLayout>
+            <div className="text-center">
+                <h1 className="text-7xl logo">スケジュール管理APP</h1>
+                <p className="pt-[10vh] text-5xl">お互いのスケジュールを管理するアプリです</p>
+            </div>
+            <div className="pt-[20vh]">
+                <PrimaryBtn>ログイン</PrimaryBtn>
+            </div>
+        </NotLoginLayout>
     )
 }
