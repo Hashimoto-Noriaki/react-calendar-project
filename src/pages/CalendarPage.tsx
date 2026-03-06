8688697453f3481377f9d0f8ce1077e8a2a35db2
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-03-07 00:15:41 +0900

    feat: fetchした予定をカレンダーに反映させた

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,53 +1,73 @@
 import { useState, useEffect } from "react";
 import {
-    eachDayOfInterval,
-    eachWeekOfInterval,
-    endOfMonth,
-    endOfWeek,
-    getMonth,
-    startOfMonth,
+  eachDayOfInterval,
+  eachWeekOfInterval,
+  endOfMonth,
+  endOfWeek,
+  getMonth,
+  isSameDay,
+  parseISO,
+  startOfMonth,
 } from "date-fns";
-import type { DateList } from "../types/calendar";
+import type { DateList, Schedule } from "../types/calendar";
 import {
-    CalendarHeader,
-    CalendarBody,
-    CalendarNav,
+  CalendarHeader,
+  CalendarBody,
+  CalendarNav,
 } from "../shared/components/organisms";
 
 export const CalendarPage = () => {
-    const [currentDate, setCurrentDate] = useState(new Date());
-    const [dateList, setDateList] = useState<DateList>([]);
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [dateList, setDateList] = useState<DateList>([]);
 
-    useEffect(() => {
-        const fetchCalendar = async () => {
-            const res = await fetch("/api/schedules");
-            await res.json();
+  useEffect(() => {
+    const fetchCalendar = async () => {
+      const res = await fetch("/api/schedules");
+      const scheduleList: Schedule[] = await res.json();
 
-            const monthOfSundayList = eachWeekOfInterval({
-                start: startOfMonth(currentDate),
-                end: endOfMonth(currentDate),
-            });
-            const newDateList: DateList = monthOfSundayList.map((date) =>
-                eachDayOfInterval({
-                    start: date,
-                    end: endOfWeek(date),
-                }).map((date) => ({ date, schedules: [] }))
-            );
-            setDateList(newDateList);
-        };
-        fetchCalendar();
-    }, [currentDate]);
+      const monthOfSundayList = eachWeekOfInterval({
+        start: startOfMonth(currentDate),
+        end: endOfMonth(currentDate),
+      });
+      const newDateList: DateList = monthOfSundayList.map((date) =>
+        eachDayOfInterval({
+          start: date,
+          end: endOfWeek(date),
+        }).map((date) => ({ date, schedules: [] }))
+      );
 
-    return (
-        <>
-            <h1 className="font-bold text-3xl mb-5">
-                {`${getMonth(currentDate) + 1}月`}
-            </h1>
-            <CalendarNav setCurrentDate={setCurrentDate} />
-            <table className="w-[80%] border-collapse border-2 border-solid border-lime-800 table-fixed">
-                <CalendarHeader />
-                <CalendarBody currentDate={currentDate} dateList={dateList} />
-            </table>
-        </>
-    );
+      // fetchした予定を対応する日付のセルに格納
+      scheduleList.forEach((schedule) => {
+        const scheduleDate = parseISO(schedule.date);
+        const firstIndex = newDateList.findIndex((oneWeek) =>
+          oneWeek.some((item) => isSameDay(item.date, scheduleDate))
+        );
+        if (firstIndex === -1) return;
+        const secondIndex = newDateList[firstIndex].findIndex((item) =>
+          isSameDay(item.date, scheduleDate)
+        );
+        newDateList[firstIndex][secondIndex].schedules = [
+          ...newDateList[firstIndex][secondIndex].schedules,
+          schedule,
+        ];
+      });
+
+      setDateList(newDateList);
+    };
+
+    fetchCalendar();
+  }, [currentDate]);
+
+  return (
+    <>
+      <h1 className="font-bold text-3xl mb-5">
+        {`${getMonth(currentDate) + 1}月`}
+      </h1>
+      <CalendarNav setCurrentDate={setCurrentDate} />
+      <table className="w-[80%] border-collapse border-2 border-solid border-lime-800 table-fixed">
+        <CalendarHeader />
+        <CalendarBody currentDate={currentDate} dateList={dateList} />
+      </table>
+    </>
+  );
 };
--- src/shared/components/atoms/index.ts
+++ src/shared/components/atoms/index.ts
@@ -1,2 +1,3 @@
 export { Input } from './Input'
 export { PrimaryBtn } from './PrimaryBtn'
+export { ScheduleBtn } from "./ScheduleBtn";
